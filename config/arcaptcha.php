a5bacccf2e01d33e3f1705598096fc16b90a8c6e
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-08-09 17:07:58 +0430

    add config file

---
+++ config/arcaptcha.php
@@ -0,0 +1,21 @@
+<?php
+
+
+return[
+    /**
+     *
+     * The site key
+     * get site key @  arcaptcha.ir/dashboard
+     *
+     */
+    'site_key'                 => env('ARCAPTCHA_SITE_KEY', ''),
+
+    /**
+     *
+     * The secret key
+     * get secret key @ arcaptcha.ir/dashboard
+     *
+     */
+    'secret_key'               => env('ARCAPTCHA_SECRET_KEY', ''),
+
+];
