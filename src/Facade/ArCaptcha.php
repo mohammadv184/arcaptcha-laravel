15e148db03d70d38fe2dd8f3b16626573c1a54ed
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-08-09 17:09:44 +0430

    Add ArCaptcha Facade

---
+++ src/Facade/ArCaptcha.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Mohammadv184\ArCaptcha\Laravel\Facade;
+
+use Illuminate\Support\Facades\Facade;
+use phpDocumentor\Reflection\Types\Boolean;
+
+/**
+ * @method static string getScript()
+ * @method static string getWidget()
+ * @method static Boolean verify(string $challenge_id)
+ */
+class ArCaptcha extends Facade
+{
+    /**
+     * getFacadeAccessor method
+     * @return string
+     */
+    protected static function getFacadeAccessor(): string
+    {
+        return 'arcaptcha';
+    }
+}
