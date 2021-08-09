c062b1b6e7073bcf45fd8e55449c6c9ba2ca5f83
Mohammad Abbasi <mohammadv184@users.noreply.github.com>
2021-08-09 12:43:26 +0000

    Apply fixes from StyleCI

--- config/arcaptcha.php
+++ config/arcaptcha.php
@@ -1,20 +1,15 @@
 <?php
 
-
 return[
     /**
-     *
      * The site key
-     * get site key @  arcaptcha.ir/dashboard
-     *
+     * get site key @  arcaptcha.ir/dashboard.
      */
     'site_key'                 => env('ARCAPTCHA_SITE_KEY', ''),
 
     /**
-     *
      * The secret key
-     * get secret key @ arcaptcha.ir/dashboard
-     *
+     * get secret key @ arcaptcha.ir/dashboard.
      */
     'secret_key'               => env('ARCAPTCHA_SECRET_KEY', ''),
 
